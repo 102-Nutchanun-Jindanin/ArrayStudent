949042a9d45df812b201f7d9a62f6f6fca2cbbc4
102-Nutchanun-Jindanin <s68122250102@ssru.ac.th>
2026-01-08 23:39:59 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String studentId;
+    private String name;
+    private String major;
+    private double gpa;
+    public static int studentCount = 0;
+    
+    public Student(String studentId, String name, String major, double gpa) {
+        this.studentId = studentId;
+        this.name = name;
+        this.major = major;
+        this.gpa = gpa;
+        studentCount++;
+    }
+    public void displayInfo() {
+        System.out.println("ID : " + studentId + " Name : " + name + " Major : " + major + " GPA : " + gpa);
+    }
+}
+//68122250102 ณัฐชนันท์ จินดานิล 
